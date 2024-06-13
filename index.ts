cf915a10e54611b2d2b10ef9c1a22b9a496ed4d8
KhalidGhani333 <khalidghani.333@gmail.com>
2024-06-13 16:33:13 +0500

    push code to github

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+//Get Input form user
+const userInput = await inquirer.prompt({
+    name:"num1",
+    type:"number",
+    message:"Please Enter Your Number:"
+})
+
+// User Input Value
+let userNum:number = userInput.num1
+
+// Logic For Table
+for(let i = 1; i <= 10; i++){
+    let result = userNum * i
+    console.log(`${userNum} x ${i} = ${result}`);
+    }
+
